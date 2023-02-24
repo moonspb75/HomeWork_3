025ef4cf78636903ca31fe327fe762e715bb2df2
Yanina <moonspb75@mail.ru>
2023-02-24 08:11:50 +0300

    HW3

---
+++ task_1_Palindrom/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом.
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+System.Console.Write("Введите целое число или текст: ");
+string number = Console.ReadLine()?? String.Empty;;
+int length = number.Length;
+// string txtToCheck = number.Replace(""," "); // для проверки текста на палиндром
+bool IsPalindrome()
+{
+    for (int i = 0; i < length / 2; i ++)
+    while(number[i] == number[length - 1])
+
+{
+    return true;
+}
+return false;
+}
+
+if (IsPalindrome()) System.Console.WriteLine($"Введенный текст/число - {number} - является палиндромом");
+else System.Console.WriteLine($"Число {number} не является палиндромом");
+System.Console.WriteLine();
